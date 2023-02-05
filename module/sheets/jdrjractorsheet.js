208973bce24b379edcfdf184ff38683a9d789d45
Vharung <contact@alexandre-crochet.fr>
2023-02-05 14:46:35 +0100

    Update jdrjractorsheet.js

--- module/sheets/jdrjractorsheet.js
+++ module/sheets/jdrjractorsheet.js
@@ -113,8 +113,10 @@
                 z=table[i].result;
             }
         } 
+        var total=parseInt(z)
+        var succes=""; 
 
-        const texte = '<img src="'+img+'"  width="24" height="24"/><span style="left: 5px;top: -7px;position: relative;font-size: 1.2em;">Jet de ' + name + ' : <span style="color: #fff;background: #23221d;padding: 5px;">' +total+'</span></span>' ;//+" - "+succes+" réussite(s)";
+        const texte = '<span style="font-size: 1.2em;">Jet de ' + name + ' : <span style="color: #fff;background: #23221d;padding:2px 5px;">' +total+'</span></span>' ;//+" - "+succes+" réussite(s)";
         roll.toMessage({
             speaker: ChatMessage.getSpeaker({ actor: this.actor }),
             flavor: texte
