5abd12ea6652837c6ac6f44d4c8c1c422a8dc76a
Vharung <93044724+Vharung@users.noreply.github.com>
2025-02-22 11:22:06 +0100

    mise à jour

--- module/sheets/jdrjractorsheet.js
+++ module/sheets/jdrjractorsheet.js
@@ -4,10 +4,10 @@
  */
  export class jdrjrActorSheet extends ActorSheet {
     static get defaultOptions() {
-        return foundry.utils.mergeObject(super.defaultOptions, {
+        return mergeObject(super.defaultOptions, {
           classes: ["jdrjr", "sheet", "actor"],
           width: 1030, //defini la auteur et la largeurs de la fiche de perso
-          height: 750,
+          height: 550,
           tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }]
         });
     }
@@ -100,35 +100,28 @@
     }
 
     //lancer de dés
-    async _onRoll(event){
+    _onRoll(event){
         let monJetDeDes = event.target.dataset["dice"];
         let nbdes = event.target.dataset["attdice"];
         const name = event.target.dataset["name"];
         const jetdeDesFormule = nbdes+"d6"; //formule du lancer
 
-        let r = await new Roll(nbdes+"d6").evaluate();
-        console.log(r)
+        let r = new Roll(nbdes+"d6");
+        var roll=r.evaluate({"async": false});
         var table=r.terms[0].results
-        console.log(table)
-        var total='';
-        let z='';
+        var z=0;
         for (var i = table.length - 1; i >= 0; i--) {
-                z =table[i].result;
-                console.log(z)
-                let color="#23221d";
-                if(z>=5){
-                    color="green"
-                }
-                total+='<span style="color: #fff;background:'+color+' ;padding:2px 5px;margin-left:3px">'+z+'</span>';        
+            if(table[i].result>z){
+                z=table[i].result;
+            }
         } 
-        
+        var total=parseInt(z)
         var succes=""; 
 
-        const texte = '<span style="font-size: 1.2em;">Jet de ' + name + ' : ' +total+'</span>' ;//+" - "+succes+" réussite(s)";
-        await r.toMessage({
-            user: game.user._id,
+        const texte = '<span style="font-size: 1.2em;">Jet de ' + name + ' : <span style="color: #fff;background: #23221d;padding:2px 5px;">' +total+'</span></span>' ;//+" - "+succes+" réussite(s)";
+        roll.toMessage({
             speaker: ChatMessage.getSpeaker({ actor: this.actor }),
-            content: texte
+            flavor: texte
         });
     }
 }
